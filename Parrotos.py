aa6538fb3c5948a6fd395129bde3bcbb5ae2d0d0
GogoZin <47559665+GogoZin@users.noreply.github.com>
2019-08-11 23:00:10 +0800

    Update Parrotos.py

--- Parrotos.py
+++ Parrotos.py
@@ -1,10 +1,13 @@
+##################
+# Code By GogoZin#
+##################
 import os
 os.system("sudo apt-get install figlet &&pip3 install requests")
 
 import requests
 word = str(input("Messege You Want To Show On Terminal : "))
 q = "figlet -f slant " + str(word) + "\r\n"
-r = requests.get("https://gist.githubusercontent.com/rickdaalhuizen90/d1df7f6042494b982db559efc01d9557/raw/488d28c1b614617025b6dc9d8da1075eedb892d4/.bashrc")
+r = requests.get("https://gist.githubusercontent.com/rickdaalhuizen90/d1df7f6042494b982db559efc01d9557/raw/488d28c1b614617025b6dc9d8da1075eedb892d4/.bashrc") # Code By GogoZin
 with open('.bashrc', 'wb') as f:
 	f.write(q.encode())
 	f.write(r.content)
